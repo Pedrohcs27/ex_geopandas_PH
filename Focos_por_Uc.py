ebc2648cf1ff6da343e993eee9416ac2f5c2b5b1
Pedro Henrique de Castro e Silva <69044793+Pedrohcs27@users.noreply.github.com>
2022-01-14 12:44:09 -0300

    Update Focos_por_Uc.py

--- Focos_por_Uc.py
+++ Focos_por_Uc.py
@@ -1,36 +1,30 @@
-# Instalação das bibliotecas.
-
-!apt install libspatialindex-dev
-!pip install rtree
-!pip install --upgrade geopandas
-!pip install --upgrade mapclassify
-!pip install descartes
 #Importação das Biliotecas.
 
 import geopandas as gpd
-import pandas as pd
 import fiona
 import matplotlib.pyplot as plt
 
 
-# Leitura do shp de focos de 2019 de incêndio no estado do RJ baixados do BDqueimadas/INPE.
-# Leitura do shp de Unidades de Conservação(GeoPortal Inea).
-focos_2019 = gpd.read_file('/content/drive/MyDrive/Colab Notebooks/Focos_24h/Focos_2019-01-01_2019-12-31.shp')
-Ucs = gpd.read_file('/content/drive/MyDrive/Gabriel_GERGET/gpl_ucs_estaduais_dez20.shp')
+# Leitura do shp de focos de 2019 de incêndio no estado do RJ baixados do BDqueimadas/INPE(https://queimadas.dgi.inpe.br/queimadas/bdqueimadas/).
+# Leitura do shp de Unidades de Conservação baixado do GeoPortal Inea(https://www.arcgis.com/apps/MapSeries/index.html?appid=00cc256c620a4393b3d04d2c34acd9ed).
+focos_2019 = gpd.read_file('Focos_2019-01-01_2019-12-31.shp')
+Ucs = gpd.read_file('gpl_ucs_estaduais_dez20.shp')
 
 # Conversão dos limites das UCS e dos focos para coordenadas geográficas com datun wgs84.
 Ucs = Ucs.to_crs("EPSG:4326")
 focos = focos_2019.to_crs("EPSG:4326")
 
-#Define função que gera o plot dos focos por UC, e salva o plot, os limites da Uc e os focos no formato kml. 
+#Define função que:
+# 1 - gera o plot e salva em png os focos por UC  
+# 2 - salva os limites da Uc e os focos no formato kml. 
 
 def PlotSaveFocosUc(Ucs, focos):
   for i in Ucs.index:
     Uc = Ucs[Ucs.index == i]# Define a UC.
     foco_uc = gpd.overlay(focos, Uc, how='intersection', keep_geom_type=False)# Faz a intercessão dos focos com a UC.
     if foco_uc.empty == True:
-      print(f'A UC {Uc.rotulo[i]} não possui focos dentro dos seus limites')# Se o resultado da intercessão for vazio printa a msg padrão com 
-    else:                                                                   #o nome da Uc.
+      print(f'A UC {Uc.rotulo[i]} não possui focos dentro dos seus limites')# Se o resultado da intercessão for vazio printa a msg padrão com o nome da Uc.
+    else:                                                                   
       #Plota e salva o plot no formato .png
       ax = Uc.plot(color='white', edgecolor='k')
       foco_uc.plot(ax=ax, marker='x', color='red', markersize=5)
